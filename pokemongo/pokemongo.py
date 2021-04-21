e33b01b7d53c4594b2f19cc2ad19c72ea89d1b48
titancoolz <basheeruddin112@gmail.com>
2021-04-21 22:07:26 +0530

    completed

--- pokemongo/pokemongo.py
+++ pokemongo/pokemongo.py
@@ -1,5 +1,7 @@
 import csv
   
+from itertools import combinations
+from django.db.models.aggregates import Count
 # csv file name
 filename = "poke.csv"
   
@@ -11,18 +13,43 @@ rows = []
 with open(filename, 'r') as csvfile:
     # creating a csv reader object
     csvreader = csv.reader(csvfile)
-      
-    # extracting field names through first r zow
-    fields = next(csvreader)
+    for row in csvreader:
+        r1=[]
+        for column in row[0].split():
+            r1.append(column)
+        rows.append(r1)
+#         print("\n")
+#     # extracting field names through first r zow
+#     fields = next(csvreader)
+    rowlist=[]
+    comb = combinations(rows[1:], 3)
+    for i in list(comb):
+        row=[]
+        a,b,c=i 
+        for tup in i:
+            row.append(tup[0])
+            row.append(tup[1])
+#             print(tup[0].split()[0])
+#             row.append(tup[1])
+        count=0   
+        for count in range(2,len(a)-1):
+            row.append(max(a[count],b[count],c[count]))
+#             count=count+1
+        
+        rowlist.append(row)            
+    with open("out.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(rowlist)
+#         rowlist=rowlist.append()
+#         print (i)
   
     # extracting each data row one by one
-    for row in csvreader:
-        rows.append(row)
+#     for row in csvreader:
+#         rows.append(row)
   
     # get total number of rows
-    print("Total no. of rows: %d"%(csvreader.line_num))
+#     print("Total no. of rows: %d"%(csvreader.line_num))
   
 # printing the field names
-print('Field names are:' + ', '.join(field for field in fields))
-
+# print('Field names are:' + ', '.join(field for field in fields))
 
